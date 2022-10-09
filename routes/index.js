e3531e284f207d6f56d18c643f77e2d37dc2feb6
aharoni <aharoniyovel4@gmail.com>
2022-10-09 12:28:48 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ msg: "FOOD STACK work fine!! Heroku update: 09/10/22    1:44 a.m !" });
+    res.json({ msg: "FOOD STACK work fine!! Heroku update: 09/10/22    12:28 p.m !" });
 })
 
 module.exports = router;
