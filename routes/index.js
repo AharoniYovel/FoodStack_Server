18618dd7e681d07fa785ded1185654b2be6672bf
aharoni <aharoniyovel4@gmail.com>
2022-11-13 22:49:14 +0200

    13/11/22

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ msg: "FOOD STACK work fine!! Heroku update: 10/11/2022    14:55 p.m !" });
+    res.json({ msg: "FOOD STACK work fine!! Heroku update: 13/11/2022    22:50 p.m !" });
 })
 
 module.exports = router;
