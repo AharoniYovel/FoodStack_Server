eef8f8f622cf42f1284f5516f61e9271efea65fa
aharoni <aharoniyovel4@gmail.com>
2022-11-22 13:55:42 +0200

    cyclic upload

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ msg: "FOOD STACK work fine!! Heroku update: 13/11/2022    23:00 p.m !" });
+    res.json({ msg: "FOOD STACK work fine!! Cyclic update: 22/11/2022    13:55 p.m !" });
 })
 
 module.exports = router;
