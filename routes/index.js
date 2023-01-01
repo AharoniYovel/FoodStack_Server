499fa94f74f69b166c62d0655d5e3d275ec269bf
aharoni <aharoniyovel4@gmail.com>
2023-01-01 21:31:48 +0200

    1/1/2023

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ msg: "FOOD STACK work fine!! Cyclic update: 22/11/2022    15:47 p.m !" });
+    res.json({ msg: "FOOD STACK work fine!! Render update: 01/01/2023    21:31 p.m !" });
 })
 
 module.exports = router;
